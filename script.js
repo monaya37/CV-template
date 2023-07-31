9f76d3f08032cb4daa2d726b4c77428f4c558c61
Khaira Bah <Monayaa1637@gmail.com>
2023-07-31 14:33:04 -0700

    final touches

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let finalValue1 = 0;
 
 function changeWidth1(){
     progress1.style.width = `${(finalValue1/100)*100}%`;
-    // progress.innerText = `${Math.ceil(((finalValue/100)*100))}%`;
 }
 
 input1.addEventListener("keyup", function(){
@@ -22,7 +21,6 @@ let finalValue12= 0;
 
 function changeWidth2(){
     progress2.style.width = `${(finalValue2/100)*100}%`;
-    // progress.innerText = `${Math.ceil(((finalValue/100)*100))}%`;
 }
 
 input2.addEventListener("keyup", function(){
